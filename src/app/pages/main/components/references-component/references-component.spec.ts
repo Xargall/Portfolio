5ee9614dcf5c31d681fdb3e671fe8f5bc7d316e5
Mathias Mayer <mayer.mathias@proton.me>
2026-07-24 11:35:44 +0200

    add: components

---
+++ src/app/pages/main/components/references-component/references-component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReferencesComponent } from './references-component';
+
+describe('ReferencesComponent', () => {
+  let component: ReferencesComponent;
+  let fixture: ComponentFixture<ReferencesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ReferencesComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ReferencesComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/main/components/references-component/references-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-references-component',
+  imports: [],
+  templateUrl: './references-component.html',
+  styleUrl: './references-component.scss',
+})
+export class ReferencesComponent {}
