914f8ff42bce6288eef3b0e7bfde8e13ca4699f6
Mathias Mayer <mayer.mathias@proton.me>
2026-07-24 12:21:27 +0200

    add: basic footer styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
+import { Footer } from './shared/components/footer/footer';
+import { Main } from "./pages/main/main";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Footer, Main],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
