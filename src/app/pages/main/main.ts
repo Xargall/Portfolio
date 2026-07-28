f983615c60bfe278a1a2f78237c26564a6227394
Mathias Mayer <mayer.mathias@proton.me>
2026-07-28 07:37:03 +0200

    add: about-me section

--- src/app/pages/main/main.ts
+++ src/app/pages/main/main.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './components/hero-component/hero-component';
+import { AboutMeComponent } from './components/about-me-component/about-me-component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeroComponent],
+  imports: [HeroComponent, AboutMeComponent],
   templateUrl: './main.html',
   styleUrl: './main.scss',
 })
