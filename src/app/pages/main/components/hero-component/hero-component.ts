dca50f52a549e77242bfd29655cf6aa15957604f
Mathias Mayer <mayer.mathias@proton.me>
2026-08-06 10:41:53 +0200

    add: translation hero

--- src/app/pages/main/components/hero-component/hero-component.ts
+++ src/app/pages/main/components/hero-component/hero-component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Button } from '../../../../shared/button/button';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hero-component',
-  imports: [Button],
+  imports: [Button, TranslatePipe],
   templateUrl: './hero-component.html',
   styleUrl: './hero-component.scss',
 })
 export class HeroComponent {
-  items = ['Available for remote work', 'Fullstack Developer', 'Based in Leipzig', 'Open to work'];
+  items = ['hero.marquee.remote', 'hero.marquee.role', 'hero.marquee.location', 'hero.marquee.open'];
   doubled = [...this.items, ...this.items, ...this.items];
 }
