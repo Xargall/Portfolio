d1f7b93b110774b388b3fd88357e760077da9e14
Mathias Mayer <mayer.mathias@proton.me>
2026-08-06 10:41:43 +0200

    add: translation header

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,25 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { Logo } from '../../logo/logo';
+import { TranslateService, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  imports: [Logo],
+  imports: [Logo, TranslatePipe],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
-  currentLang = 'en';
+  private translate = inject(TranslateService);
+  currentLang = signal<'en' | 'de'>('en');
   activeLink: string = '';
   isMenuOpen = signal(false);
 
   changeLanguage(lang: 'en' | 'de') {
-    this.currentLang = lang;
-
-    // ngx-translate
-    // this.translate.use(lang);
-
-    // oder Transloco
-    // this.transloco.setActiveLang(lang);
+    this.currentLang.set(lang);
+    this.translate.use(lang);
   }
 
   setActive(link: string) {
